d3663b87f9e899e01b16b231ccf57d001bb8c82a
minwooWi <47047004+minwooWi@users.noreply.github.com>
2024-02-14 23:41:00 +0900

    Time: 7 ms (27.24%), Space: 44.3 MB (16.81%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,18 +1,13 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        // Convert the integer to a string
-        String numberAsString = String.valueOf(x);
-
-        // Create an array to store the characters of the string
-        char[] characters = numberAsString.toCharArray();
-
-        // Create a second array to store the characters of the string in reverse order
-        char[] reversedCharacters = new char[characters.length];
-        for (int i = characters.length - 1, j = 0; i >= 0; i--, j++) {
-            reversedCharacters[j] = characters[i];
+        String intToString = String.valueOf(x);
+        char[] stringArray = intToString.toCharArray();
+        char[] reverseArray = new char[stringArray.length];
+        
+        for(int i=stringArray.length-1, j=0; i >= 0; i--, j++){
+            reverseArray[j] = stringArray[i];
         }
-
-        // Compare the two arrays
-        return Arrays.equals(characters, reversedCharacters);
+        
+        return Arrays.equals(stringArray, reverseArray);
     }
 }
